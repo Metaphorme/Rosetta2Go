1e9fe5336071fac697d576c6dcbe219d29f33e9b
Diazepam <102651183+Metaphorme@users.noreply.github.com>
2022-11-30 21:49:35 +0800

    Only expose fileserver to localhost.

--- build4singularity.sh
+++ build4singularity.sh
@@ -2,6 +2,6 @@
 
 echo "Start to build for singularity..."
 
-/usr/bin/python3 -m http.server 28293 &
+/usr/bin/python3 -m http.server --bind 127.0.0.1 28293 &
 singularity build --fakeroot rosetta.sif Singularity.def
 kill -9 $(netstat -antp | grep :28293 | awk '{print $7}' | awk -F'/' '{ print $1 }')
