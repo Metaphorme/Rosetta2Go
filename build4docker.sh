982e54fc971266aa7d1832f282db1f62cbb7ed9f
Diazepam <102651183+Metaphorme@users.noreply.github.com>
2022-11-30 14:56:10 +0800

    Build with host network.

--- build4docker.sh
+++ build4docker.sh
@@ -3,5 +3,5 @@
 echo "Start to build for docker..."
 
 /usr/bin/python3 -m http.server 28294 &
-docker build -t rosetta - < Dockerfile
+docker build --network="host" -t rosetta .
 kill -9 $(netstat -antp | grep :28294 | awk '{print $7}' | awk -F'/' '{ print $1 }')
