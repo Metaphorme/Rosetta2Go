ff5e38c36a9ab4fcd57c47a7d8f7f059cc6d32dd
Diazepam <102651183+Metaphorme@users.noreply.github.com>
2022-12-01 09:48:55 +0800

    Transport Dockerfile only to Docker.

--- build4docker.sh
+++ build4docker.sh
@@ -3,5 +3,5 @@
 echo "Start to build for docker..."
 
 /usr/bin/python3 -m http.server --bind 127.0.0.1 28294 &
-docker build --network="host" -t rosetta .
+docker build --network="host" -t rosetta - < Dockerfile
 kill -9 $(netstat -antp | grep :28294 | awk '{print $7}' | awk -F'/' '{ print $1 }')
